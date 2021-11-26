aa67cd4cde4f05573a43def69c24e23d0dd348b5
Suryo Mujahid <suryomujahid@gmail.com>
2021-11-26 17:57:37 +0700

    feat(admin, role): dynamic admin dashboard, add multi role route

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -23,7 +23,7 @@
                     <div class="icon-in-bg bg-indigo text-white rounded-circle"><i class="icon-user"></i></div>
                     <div class="ml-4">
                         <span>Akun Siswa</span>
-                        <h4 class="mb-0 font-weight-medium">3M</h4>
+                        <h4 class="mb-0 font-weight-medium total-student">0</h4>
                     </div>
                 </div>
             </div>
@@ -36,7 +36,7 @@
                     <div class="icon-in-bg bg-azura text-white rounded-circle"><i class="icon-user"></i></div>
                     <div class="ml-4">
                         <span>Akun Guru</span>
-                        <h4 class="mb-0 font-weight-medium">500K</h4>
+                        <h4 class="mb-0 font-weight-medium total-teacher">0</h4>
                     </div>
                 </div>
             </div>
@@ -49,7 +49,7 @@
                     <div class="icon-in-bg bg-orange text-white rounded-circle"><i class="icon-user"></i></div>
                     <div class="ml-4">
                         <span>Akun Admin</span>
-                        <h4 class="mb-0 font-weight-medium">100K</h4>
+                        <h4 class="mb-0 font-weight-medium total-admin">0</h4>
                     </div>
                 </div>
             </div>
@@ -62,7 +62,7 @@
                     <div class="icon-in-bg bg-success text-white rounded-circle"><i class="icon-user"></i></div>
                     <div class="ml-4">
                         <span>Total Akun</span>
-                        <h4 class="mb-0 font-weight-medium">3,6M</h4>
+                        <h4 class="mb-0 font-weight-medium">{{count($users)}}</h4>
                     </div>
                 </div>
             </div>
@@ -88,14 +88,14 @@
                         <div class="d-flex justify-content-start mt-3">
                             <div class="mr-5">
                             <label class="mb-0">Siswa</label>
-                            <h4>9,231</h4>
+                            <h4 class="total-student">0</h4>
                             </div>
                             <div>
                             <label class="mb-0">Guru</label>
-                            <h4>10,850</h4>
+                            <h4 class="total-teacher">0</h4>
                             </div>
                         </div>
-                        <div id="chart-donut" style="height: 250px"></div>
+                        <div id="chart-donut-stats" style="height: 250px"></div>
                     </div>
                     <div class="col-lg-8 col-md-12 col-sm-12">
                         <div id="flotChart" class="flot-chart"></div>
@@ -113,4 +113,58 @@
 <script src="{{asset('assets/bundles/knob.bundle.js')}}"></script>
 
 <script src="{{asset('assets/js/index4.js')}}"></script>
+
+<script>
+    const users = {!! json_encode($users) !!};
+    let totalAdmin = 0;
+    let totalTeacher = 0;
+    let totalStudent = 0;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i]['role'] === 'ADMIN'){
+            totalAdmin += 1;
+        }
+        if (users[i]['role'] === 'TEACHER'){
+            totalTeacher += 1;
+        }
+        if (users[i]['role'] === 'STUDENT'){
+            totalStudent += 1;
+        }
+    }
+
+    $(function() {
+        $('.total-admin').html(totalAdmin);
+        $('.total-teacher').html(totalTeacher);
+        $('.total-student').html(totalStudent);
+    });
+
+    c3.generate({
+        bindto: '#chart-donut-stats',
+        data: {
+            columns: [
+                ['data1', totalTeacher],
+                ['data2', totalStudent]
+            ],
+            type: 'donut',
+            colors: {
+                'data1': '#9367B4',
+                'data2': '#17C2D7',
+            },
+            names: {
+                // name of each serie
+                'data1': 'Guru',
+                'data2': 'Murid'
+            }
+        },
+        axis: {
+        },
+        legend: {
+            show: false,
+        },
+        padding: {
+            bottom: 20,
+            top: 0
+        },
+    });
+</script>
 @endsection
